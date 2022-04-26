0c0fe6d70fe74e8602f20deaa7a8e0dd4e425afe
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-04-26 18:17:44 +0900

    ユーザの情報取得クエリ作成

--- app/app.js
+++ app/app.js
@@ -4,12 +4,45 @@ const sqlite3 = require("sqlite3");
 const dbPath = "app/db/database.sqlite3";
 
 //URLの設定と返すデータ
+/**
+ * ユーザ全員の情報を取得.
+ */
 app.get("/api/v1/users", (req, res) => {
+  //DBへの接続
   const db = new sqlite3.Database(dbPath);
+  //SQL文
   db.all("SELECT * FROM users;", (err, rows) => {
     res.json(rows);
   });
-  db.close;
+  db.close();
+});
+
+/**
+ * ユーザ個人の情報を取得.
+ */
+app.get("/api/v1/users/:id", (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+  //URLからIDの取得
+  const id = req.params.id;
+  //データを全て取得しない場合は「row」
+  db.get(`SELECT * FROM users WHERE id = ${id};`, (err, row) => {
+    res.json(row);
+  });
+  db.close();
+});
+
+/**
+ * 検索.
+ */
+app.get("/api/v1/search", (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+  const keyword = req.query.q;
+
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+    res.json(rows);
+  });
+
+  db.close();
 });
 
 //envに指定があればそれ、無ければ3000でポート立ち上げ
