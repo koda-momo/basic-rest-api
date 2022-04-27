3623f4f89babd0f5934ba3dbc05963183fd8526a
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-04-27 13:21:28 +0900

    検索メソッド追加

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,7 +1,14 @@
 // index.htmlを読み込んだ時に最初に必要なメソッドを発火
 //jsファイルで作成したメソッドの呼び出し.
-
 const indexModule = (() => {
-  //usersモジュールの呼び出し
+  /**
+   * 検索ボタン押下で呼び出し.
+   */
+  document.getElementById("search-btn").addEventListener("click", () => {
+    return searchModule.searchUsers();
+  });
+  /**
+   * usersモジュールの呼び出し.
+   */
   return usersModule.fetchAllUsers();
 })();
--- app/public/js/search.js
+++ app/public/js/search.js
@@ -1,1 +1,30 @@
 //検索メソッド用
+const searchModule = (() => {
+  //URLの設定
+  const BASE_URL = "http://localhost:3000/api/v1/search";
+
+  return {
+    searchUsers: async () => {
+      //inputに入力したデータを取得
+      const query = document.getElementById("search").value;
+      const res = await fetch(`${BASE_URL}?q=${query}`);
+
+      //整形
+      const result = await res.json();
+      let body = "";
+      for (let i = 0; i < result.length; i++) {
+        const user = result[i];
+        body += `<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.profile}</td>
+        <td>${user.date_of_birth}</td>
+        <td>${user.created_at}</td>
+        <td>${user.updated_at}</td>
+        </tr>`;
+      }
+      //bodyを置き換え
+      document.getElementById("users-list").innerHTML = body;
+    },
+  };
+})();
