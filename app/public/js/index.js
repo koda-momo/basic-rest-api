f5decfe8f8baf4716b732dbdb187ebc430007637
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-04-27 12:19:44 +0900

    usersモジュール作成

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,1 +1,7 @@
 // index.htmlを読み込んだ時に最初に必要なメソッドを発火
+//jsファイルで作成したメソッドの呼び出し.
+
+const indexModule = (() => {
+  //usersモジュールの呼び出し
+  return usersModule.fetchAllUsers();
+})();
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -1,1 +1,31 @@
 //ユーザ情報用
+//モジュール化(usersでのみ使用するものの作成)
+const usersModule = (() => {
+  //URLの設定
+  const BASE_URL = "http://localhost:3000/api/v1/users";
+
+  return {
+    //API呼び出し
+    fetchAllUsers: async () => {
+      //fetch(URL)でGETになる
+      const res = await fetch(BASE_URL);
+      //JSで扱えるJSON型に変換
+      const users = await res.json();
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        const body = `<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.profile}</td>
+        <td>${user.date_of_birth}</td>
+        <td>${user.created_at}</td>
+        <td>${user.updated_at}</td>
+        </tr>`;
+        //HTMLのid「users-list」のところの後ろに上記を追加
+        document
+          .getElementById("users-list")
+          .insertAdjacentHTML("beforeend", body);
+      }
+    },
+  };
+})();
